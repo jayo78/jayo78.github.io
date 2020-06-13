c07154d7934639265848c78f142881d96c6851dc
jayo78 <jaysonhurst00@gmail.com>
2020-06-12 21:14:45 -0400

    update

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "Learning x86 with the winAPI",
+        "excerpt":"This is a short overview of the x86 assembly instruction set in a windows environment for complete beginners. We will tediously walk through a “helloworld” program to cover how windows functions can be called in assembly. While most native applications (.exe) are written in high level languages like C/C++ and...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/Learning-x86-with-the-winAPI/",
+        "teaser": null
+      }]
